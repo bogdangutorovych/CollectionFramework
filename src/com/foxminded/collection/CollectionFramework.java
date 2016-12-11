d8615c268f055fea4a15a6efb573165f60db064c
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2016-12-11 15:13:03 +0200

    Task 5 v.2

--- src/com/foxminded/collection/CollectionFramework.java
+++ src/com/foxminded/collection/CollectionFramework.java
@@ -1,28 +1,33 @@
 package com.foxminded.collection;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CollectionFramework {
+	
+	private Map<String, Integer> map;
 
+	public CollectionFramework() {
+		this.map = new HashMap<String, Integer>();
+	}
+	
 	public int calculateUniqueSymbolQuantity(String string) {
 		char[] symbols = string.toCharArray();
 		ArrayList<Character> charList = new ArrayList<Character>();
 		for (char c : symbols) {
-			if (charList.contains(c) == false) {
+			if (!charList.contains(c)) {
 				charList.add(c);
 			}
 		}
 		return charList.size();
 	}
 
-	public int checkMap(String string, Map<String, Integer> map) {
-		if (map.containsKey(string)) {
-			System.out.println("Output result from Map");
-			return (int) map.get(string);
+	public int checkMap(String string) {
+		if (this.map.containsKey(string)) {
+			return (int) this.map.get(string);
 		}
-		map.put(string, calculateUniqueSymbolQuantity(string));
-		System.out.println("Insert result to Map");
+		this.map.put(string, calculateUniqueSymbolQuantity(string));
 		return calculateUniqueSymbolQuantity(string);
 	}
 	
