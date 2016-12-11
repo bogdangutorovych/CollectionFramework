d365d41a62ead296875c811b247e98715a0d287c
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2016-12-11 15:15:35 +0200

    Task 5 v.2 all

--- src/com/foxminded/collection/Main.java
+++ src/com/foxminded/collection/Main.java
@@ -1,7 +1,5 @@
 package com.foxminded.collection;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -9,30 +7,29 @@ public class Main {
 	public static void main(String[] args) {
 
 		CollectionFramework collectionFramework = new CollectionFramework();
-		Map<String, Integer> map = new HashMap<String, Integer>();
 		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Input initial text: ");
 		String inputText = s.nextLine();
 
-		System.out.println("String: " + inputText + " - " + collectionFramework.checkMap(inputText, map));
+		System.out.println("String: " + inputText + " - " + collectionFramework.checkMap(inputText));
 		
 		String string = "";
-		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string, map));
+		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string));
 		
-		System.out.println(inputText + " - " + collectionFramework.checkMap(inputText, map));
+		System.out.println("String: " + inputText + " - " + collectionFramework.checkMap(inputText));
 		
 		string = "asdf qwer";
-		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string, map));
+		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string));
 		
 		string = "asdf qwerrrrrr";
-		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string, map));
+		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string));
 		
 		string = "asdf qwerrrrrr";
-		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string, map));
+		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string));
 		
 		string = "asdf    ";
-		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string, map));
+		System.out.println("String: " + string + " - " + collectionFramework.checkMap(string));
 		
 	}
 
